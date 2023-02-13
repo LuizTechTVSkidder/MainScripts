1f75594c488584a2b4a922e82f604be935753267
LuizTechTVSkidder <124854300+LuizTechTVSkidder@users.noreply.github.com>
2023-02-13 02:41:22 -0300

    By LuizTechTV

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+--[[
+  THIS SCRIPT ONLY SUPPORTS 1 GAME BC ITS A WIP AND WILL BE A HARD
+   Sonic.Exe: The Disaster
+]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuizTechTVSkidder/MainScripts/main/Sources/Las.lua"))()
