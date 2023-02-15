8b4d864bebed0bea0aeb1cd15216f1e8b3f51fb3
LuizTechTVSkidder <124854300+LuizTechTVSkidder@users.noreply.github.com>
2023-02-15 09:31:48 -0300

    By Xheptc

--- Libraries/Kavo.lua
+++ Libraries/Kavo.lua
@@ -1,3 +1,132 @@
+repeat wait() until game:IsLoaded()
+
+if syn then
+    pcall(function()
+        syn.protect_gui(game:GetService("CoreGui"))
+    end)
+end
+
+    syn_websocket_send = syn_websocket_send
+    firesignal = firesignal
+    makefolder = makefolder
+    is_protosmasher_caller = is_protosmasher_caller
+    clonefunction = clonefunction
+    setrawmetatable = setrawmetatable 
+    getrawmetatable = getrawmetatable
+    getinstancefromstate = getinstancefromstate
+    setfflag = setfflag
+    getcallingscript = getcallingscript or get_calling_script
+    getrenv = getrenv
+    syn_crypt_b64_encode = syn_crypt_b64_encode
+    newcclosure = newcclosure
+    getspecialinfo = debug.getspecialinfo or getspecialinfo
+    shared = shared
+    decompile = decompile
+    loadstring = loadstring
+    getprotos = debug.getprotos or getprotos
+    hookfunction = hookfunction or hookfunc
+    isfile = isfile
+    getproto = debug.getproto or getproto
+    print = print
+    isrbxactive = isrbxactive
+    rconsoleinfo = rconsoleinfo
+    make_readonly = make_readonly
+    getstack = debug.getstack or getstack
+    rconsolename = rconsolename
+    unlockmodulescript = unlockmodulescript
+    getupvalue = debug.getupvalue or getupvalue
+    setproto = debug.setproto or setproto
+    mouse1click = mouse1click
+    setupvalue = debug.setupvalue or setupvalue
+    getscripts = getscripts or get_scripts
+    rconsoleerr = rconsoleerr
+    dumpstring = dumpstring
+    keypress = keypress
+    syn_crypt_derive = syn_crypt_derive
+    rconsoleclear = rconsoleclear
+    is_redirection_enabled = is_redirection_enabled
+    syn_context_set = syn_context_set
+    isreadonly = isreadonly or function(tbl) return is_readonly; end;
+    mouse2click = mouse2click
+    getinfo = debug.getinfo or getinfo
+    writefile = writefile
+    loadfile = loadfile
+    getconstant = debug.getconstant or getconstant
+    is_synapse_function = is_synapse_function or issynapsefunction
+    getconnections = getconnections
+    checkcaller = checkcaller
+    setreadonly = setreadonly or function(tbl, val) if val then make_readonly(tbl); else make_writeable(tbl); end; end;
+    syn_crypt_encrypt = syn_crypt_encrypt
+    warn = warn
+    validfgwindow = validfgwindow
+    saveinstance = saveinstance
+    getinstances = getinstances or get_instances
+    getconstants = debug.getconstants or getconstants
+    getloadedmodules = getloadedmodules or get_loaded_modules
+    require = require
+    getnilinstances = getnilinstances or get_nil_instances
+    setclipboard = setclipboard
+    delfile = delfile
+    firetouchinterest = firetouchinterest
+    mouse1release = mouse1release
+    syn_websocket_close = syn_websocket_close
+    setnamecallmethod = setnamecallmethod
+    rconsoleprint = rconsoleprint
+    getsenv = getsenv
+    messagebox = messagebox
+    replaceclosure = replaceclosure
+    delfolder = delfolder
+    keyrelease = keyrelease
+    isfolder = isfolder
+    XPROTECT = XPROTECT or xprotect
+    getcallstack = getcallstack
+    appendfile = appendfile
+    syn_crypt_hash = syn_crypt_hash
+    syn_websocket_connect = syn_websocket_connect
+    is_protosmasher_closure = is_protosmasher_closure or isprotosmasherclosure
+    mousemoverel = mousemoverel
+    printconsole = printconsole
+    listfiles = listfiles or list_files
+    islclosure = islclosure or is_lclosure
+    rconsolewarn = rconsolewarn
+    getstateenv = getstateenv
+    syn_crypt_decrypt = syn_crypt_decrypt
+    readfile = readfile
+    mousescroll = mousescroll
+    mousemoveabs = mousemoverel
+    setconstant = debug.setconstant or setconstant
+    getpropvalue = getpropvalue
+    syn_crypt_b64_decode = syn_crypt_b64_decode
+    mouse2release = mouse2release
+    mouse2press = mouse2press
+    getgc = getgc
+    getstates = getstates
+    getpointerfromstate = getpointerfromstate
+    mouse1press = mouse1press
+    getnamecallmethod = getnamecallmethod or get_namecall_method
+    setpropvalue = setpropvalue
+    rconsoleinputasync = rconsoleinputasync
+    syn_crypt_random = syn_crypt_random
+    fireclickdetector = fireclickdetector
+    rconsoleinput = rconsoleinput
+    getmenv = getmenv
+    getreg = getreg or debug.getregistry or getregistry
+    getgenv = getgenv
+    messageboxasync = messageboxasync
+    getupvalues = getupvalues or debug.getupvalues or getupvalues
+    setstack = setstack or debug.setstack or setstack
+    syn_context_get = syn_context_get
+    syn_isactive = syn_isactive
+    profilebegan = debug.profilebegin or profilebegin
+    traceback = debug.traceback or traceback
+    setmetatable = debug.setmetatable or setmetatable
+    getmetatable = debug.getmetatable or getmetatable
+    getfenv = debug.getfenv or getfenv
+    setupvaluename = debug.setupvaluename or setupvaluename
+    profileend = debug.profileend or profileend
+    validlevel = debug.validlevel or validlevel
+
+
 local Kavo = {}
 
 local tween = game:GetService("TweenService")
@@ -127,7 +256,9 @@ local SettingsT = {
 }
 
 local Name = "KavoConfig.JSON"
-
+if not isfile("KavoConfig.JSON") then
+   writefile(Name, "{}") 
+end
 pcall(function()
 
 if not pcall(function() readfile(Name) end) then
@@ -228,6 +359,7 @@ function Kavo.CreateLib(kavName, themeList)
     ScreenGui.ResetOnSpawn = false
 
     Main.Name = "Main"
+    Main.Active = true
     Main.Parent = ScreenGui
     Main.BackgroundColor3 = themeList.Background
     Main.ClipsDescendants = true
