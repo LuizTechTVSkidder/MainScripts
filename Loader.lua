40998237e43742639e208947f06a19e0166c422f
LuizTechTVSkidder <124854300+LuizTechTVSkidder@users.noreply.github.com>
2023-02-19 02:30:23 -0300

    Loader Updated

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,8 @@
 --[[
-  THIS SCRIPT ONLY SUPPORTS 1 GAME BC ITS A WIP AND WILL BE A HARD
+  THIS SCRIPT ONLY SUPPORTS 1 GAME BC ITS A WIP AND WILL BE A HARD WORK
    Sonic.Exe: The Disaster
+  UPCOMING ONE:
+   Flood Escape 2
+   And More Or idk
 ]]--
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuizTechTVSkidder/MainScripts/main/Sources/Las.lua"))()
